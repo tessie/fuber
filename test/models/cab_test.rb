2d0cbba406986000a8b6adbf4429540b985433fb
tessy <tessyjosep1992@gmail.com>
2017-08-18 21:59:57 +0530

    Add test for cab model

---
+++ test/models/cab_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+require 'minitest/autorun'
+
+describe Cab do
+
+  before do
+    @cab = build(:cab)
+  end
+
+  it 'should save the object' do
+    assert @cab.save
+  end
+
+  it 'should not save cab with blank status' do
+    @cab.status = nil
+    assert !@cab.save
+  end
+
+  it 'should not save cab with blank registration number' do
+    @cab.registration_number = nil
+    assert !@cab.save
+  end
+
+  it 'should not save cab with blank latitude' do
+    @cab.lat = nil
+    assert !@cab.save
+  end
+
+  it 'should not save cab with blank long' do
+    @cab.long = nil
+    assert !@cab.save
+  end
+
+  it 'should not save when status is not booked/available' do
+    @cab.status = 'abdc'
+    assert !@cab.save
+  end
+end
