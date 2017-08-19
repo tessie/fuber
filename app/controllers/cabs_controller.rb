4a6067026aaac5ed68dd82681b31313f120f090e
tessy <tessyjosep1992@gmail.com>
2017-08-19 15:47:04 +0530

    Cab Control list all, list nearest, book

--- app/controllers/cabs_controller.rb
+++ app/controllers/cabs_controller.rb
@@ -1,14 +1,29 @@
 class CabsController < ApplicationController
 
   def index
-    render json:  { status: 'success' }
+    cabs = Cab.available_cabs
+    render json: { status: 'success', cabs: cabs }
   end
 
   def nearest
-    render json:  { status: 'success' }
+    if params[:long].blank? || params[:lat].blank?
+      response = { status: 'failure', message: 'Please provide your current coordinates' }
+    else
+      color = params[:color].blank? ? nil : params[:color].to_s
+      cab = Cab.nearest(params[:loc].to_f, params[:lat].to_f, color)
+      message = cab.nil? ? 'Sorry! No cabs are available' :  ' '
+      response = { status: 'success', details: cab, message: message }
+    end
+    render json: response
   end
 
   def book
-    render json:  { status: 'success' }
+    @cab = Cab.find_by_id params[:id]
+    if @cab.present? && @cab.book
+      response = { status: 'success', message: 'Booked Successfully', details: @cab }
+    else
+      response = { status: 'failure', message: 'Cab Not Available' }
+    end
+    render json: response
   end
 end
