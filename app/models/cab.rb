072b9c7dd377a7f2ddee61ad767d3358264781e4
tessy <tessyjosep1992@gmail.com>
2017-08-19 12:10:20 +0530

    Method to book, check availablity and get nearest car and scope for checking pink car

--- app/models/cab.rb
+++ app/models/cab.rb
@@ -9,7 +9,31 @@ class Cab < ApplicationRecord
   validates_inclusion_of :status, in: STATUS
   validates_uniqueness_of :registration_number
 
-  scope :available_cabs, -> { where( status: 'available').order(created_at: 'desc') }
+  scope :available_cabs, -> { where(status: 'available').order(created_at: 'desc') }
+  scope :pink_available_cabs, -> { available_cabs.where(color: 'pink') }
 
+  def available?
+    status == 'available'
+  end
 
+  def book
+    update_attribute(:status, 'booked') if available?
+  end
+
+  def self.car_list(color = nil)
+    color == 'pink' ? pink_available_cabs : available_cabs
+  end
+
+  def self.nearest(lat, long, color = nil)
+    nearest_cab = nil
+    shortest_distance = Float::INFINITY
+    car_list(color).each do |cab|
+      distance = Math.sqrt((lat - cab.lat)**2 + (long - cab.lat)**2)
+      if distance < shortest_distance
+        shortest_distance = distance
+        nearest_cab = cab
+      end
+    end
+    nearest_cab
+  end
 end
--- test/models/cab_test.rb
+++ test/models/cab_test.rb
@@ -40,4 +40,37 @@ describe Cab do
     @cab.save
     refute build(:cab, registration_number: @cab.registration_number).save
   end
+
+  describe '#test_book' do
+    it 'should return false when cab is already booked' do
+      cab = create(:cab, status: 'booked')
+      refute cab.book
+    end
+
+    it 'should book when a  cab is available' do
+      cab = create(:cab, status: 'available')
+      cab.book
+      assert cab.status, 'available'
+    end
+  end
+
+  describe '#test_nearest' do
+    it 'should return nil when no cabs are available' do
+      create(:cab, status: 'booked')
+      assert_nil Cab.nearest(10, 2)
+    end
+
+    it 'should return the nearest cab when there are available cabs' do
+      cab1 = create(:cab, status: 'available', lat: 2, long: 0)
+      create(:cab, status: 'available', lat: 100, long: 23, registration_number: 'KL543678')
+      assert Cab.nearest(4, 0), cab1
+    end
+
+    it 'should return the closest when pink available car when color is pink' do
+      pink_cab = create(:cab, status: 'available', lat: 3, long: 0, color: 'pink')
+      create(:cab, status: 'available', lat: 100, long: 23, registration_number: 'KL543678', color: 'pink')
+      create(:cab, status: 'available', lat: 2, long: 0, registration_number: 'KL543670')
+      assert Cab.nearest(0, 0), pink_cab
+    end
+  end
 end
